7500057cc050bb2afb97109a052c4e473f132fd8
Rohan Sharma <rohan.sharma@meesho.com>
2025-02-05 22:52:10 +0530

    a13

---
+++ ss.js
@@ -0,0 +1,11 @@
+{
+  metaInfo: {
+    title: '<I will be sanitized>',
+    meta: [{
+      vmid: 'description',
+      name: 'description',
+      content: '& I will not be <sanitized>'
+    }],
+    __dangerouslyDisableSanitizers: ['meta']
+  }
+}
